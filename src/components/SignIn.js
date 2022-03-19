e03b675bb2a2f17f13381b36508155b8759a75ad
ZumTheZazaKing <zahidi85543@gmail.com>
2022-03-19 13:52:22 +0800

    Advertised mobile version on sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,13 @@
-export function SignIn(){
+import AndroidIcon from '@mui/icons-material/Android';
 
+export function SignIn(){
     return (<div id="SignIn">
         <h1>Welcome To Notiz</h1>
         <br/>
         <h3>Take some notes and increase productivity</h3>
+        <br/>
+        <a href="https://mega.nz/file/Us1jGbpb#lQII2sA96GI5dXwCWaZ0Big07WWTBI5tBsLk_g3GGE8" target="_blank" rel="noreferrer">
+            <span>DOWNLOAD THE APP</span> <AndroidIcon />
+        </a>
     </div>)
 }
